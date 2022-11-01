8a46959b61749c8e2704f9f893b0817e1c9a388a
Nima Cortez <cortez.nima@gmail.com>
2022-11-01 00:46:36 +0000

    adding play

---
+++ play.js
@@ -0,0 +1,19 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "10.0.2.15", 
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+conn.on("data", (information) => {
+  console.log(information);
+});
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
