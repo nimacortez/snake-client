d09f0eb58063439618321804c6e1c5699bcb8623
Nima Cortez <cortez.nima@gmail.com>
2022-11-01 03:22:53 +0000

    adding client

--- client.js
+++ client.js
@@ -3,4 +3,17 @@ const connect = function () {
     host: "10.0.2.15", 
     port: 50541
   });
-}
+
+conn.setEncoding('utf8');
+
+conn.on("connect", () => {
+  console.log("Successfully connected to game server!");
+  conn.write("Name: LHL");
+});
+
+conn.on("data", information => {
+  console.log(information);
+});
+
+return conn;
+};
