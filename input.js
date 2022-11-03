8eb7e13e9c3d93f9af82ded056f97ab230f506d4
Nima Cortez <cortez.nima@gmail.com>
2022-11-03 15:51:31 +0000

    adding input

---
+++ input.js
@@ -0,0 +1,21 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  console.log("This is working.", data);
+  if (data === "\u0003") {
+    process.exit();
+};
+};
+
+
+setupInput();
+
+module.exports = { setupInput };
